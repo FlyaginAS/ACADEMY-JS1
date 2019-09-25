39982080c589cded3990fd3138737a0b02bd9667
ruscat <rus.reg.36@gmail.com>
2019-09-25 13:22:14 +0300

    home2  stats.js write1

--- js/stats.js
+++ js/stats.js
@@ -1,4 +1,11 @@
 'use strict';
-window.renderStatistics= function (ctx) {
+var renderCloud = function(){
 
+};
+
+window.renderStatistics= function (ctx, names, times) {
+  ctx.fillStyle='rgba(0,0,0,0.3)';
+  ctx.fillRect(110, 60, 500,200);
+  ctx.fillStyle = 'orange';
+  ctx.fillRect(100, 50, 500, 200);
 };
