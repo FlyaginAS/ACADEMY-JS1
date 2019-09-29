c7e5168e519cf38656b7da3c09ec13901dc632df
ruscat <rus.reg.36@gmail.com>
2019-09-29 16:44:26 +0300

    keksobooking generate avatar i title done

--- кексобукинг/js/map.js
+++ кексобукинг/js/map.js
@@ -1,7 +1,34 @@
 'use strict';
 
 let  adverts = [];
-let generateObj=function () {
+let arr=['01', '02', '03', '04', '05', '06', '07', '08'];
+let arr2=[
+  "Большая уютная квартира",
+  "Маленькая неуютная квартира",
+  "Огромный прекрасный дворец",
+  "Маленький ужасный дворец",
+  "Красивый гостевой домик",
+  "Некрасивый негостеприимный домик",
+  "Уютное бунгало далеко от моря",
+  "Неуютное бунгало по колено в воде"
+];
+
+function generateAvatar() {
+  let random=Math.floor(Math.random()*arr.length);
+  let elem = arr.splice(random, 1);
+  // console.log(`img/avatars/user${elem[0]}.png`);
+  return `img/avatars/user${elem[0]}.png`;
+}
+
+function generateTitle() {
+  let random=Math.floor(Math.random()*arr2.length);
+  let elem = arr2.splice(random, 1);
+  return  elem[0];
+}
+
+
+
+function generateObj() {
   return {
     author: {
       avatar: generateAvatar()
@@ -20,9 +47,11 @@ let generateObj=function () {
       photos: generatePhotos()
     },
     location: {
-
+      x: generateX(),
+      y: generateY()
     }
   };
-};
+}
+
 
 
