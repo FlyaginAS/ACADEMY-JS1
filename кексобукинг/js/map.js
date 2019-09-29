c4007cd174491d707949057f7ec5a6e88574752e
ruscat <rus.reg.36@gmail.com>
2019-09-29 17:12:31 +0300

    keksobooking  generateCheckin

--- кексобукинг/js/map.js
+++ кексобукинг/js/map.js
@@ -25,10 +25,43 @@ function generateTitle() {
   let elem = arr2.splice(random, 1);
   return  elem[0];
 }
+//проверить нижнюю функцию
+function generateAddress() {
+  return `${location.x}, ${location.y}`;
+}
+
+function generatePrice() {
+  let price =Math.floor(Math.random()*999000 + 1000) ;
+  return price;
+}
+
+function generateType() {
+  let arr = ['palace', 'flat', 'house', 'bungalo'];
+
+  let random = Math.floor(Math.random()*arr.length);
+  return arr[random];
+}
+
+function generateRooms() {
+  let random=Math.floor(Math.random()*5+1);
+  return random;
+}
+
+function generateGuests() {
+  let random=Math.floor(Math.random()*10+1);
+  return random;
+}
+
+function generateCheckin() {
+  let arr=['12:00', '13:00', '14:00'];
+  let random=Math.floor(Math.random()*arr.length);
+  return arr[random];
+}
 
 
 
-function generateObj() {
+
+function generateAdvert() {
   return {
     author: {
       avatar: generateAvatar()
