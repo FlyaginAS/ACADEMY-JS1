abf00aa6a70b3255f2c6419cde65b4c551ed9882
ruscat <rus.reg.36@gmail.com>
2019-10-03 17:10:14 +0300

    кекс перемещение метки ограничил область перемещения метки

--- кексобукинг/js/map.js
+++ кексобукинг/js/map.js
@@ -344,20 +344,33 @@ inputPrice.addEventListener('change', inputPriceChangeHandler);
 (function () {
   let mapPinMain=document.querySelector('.map__pin--main');
   mapPinMain.addEventListener('mousedown', function (evtDown) {
-    //logic
-    evt.preventDefault();
+    evtDown.preventDefault();
     let coordsStart={
       x: evtDown.clientX,
       y: evtDown.clientY
     };
-    function onPinmove(evtMove){
-      let shift={
-        x: evtMove.clientX- coordsStart.x,
-        y: evtMove.clientY - coordsStart.y
-      };
-      coordsStart.x=evtMove.clientX;
-      coordsStart.y=evtMove.clientY;
+
+    function onPinMove(evtMove){
+      if(evtMove.clientX>200 && evtMove.clientX<document.documentElement.clientWidth-20 && evtMove.clientY>50 && evtMove.clientY<640){
+        evtMove.preventDefault();
+        let shift={
+          x: evtMove.clientX- coordsStart.x,
+          y: evtMove.clientY - coordsStart.y
+        };
+        coordsStart.x=evtMove.clientX;
+        coordsStart.y=evtMove.clientY;
+        mapPinMain.style.left=mapPinMain.offsetLeft + shift.x +'px';
+        mapPinMain.style.top=mapPinMain.offsetTop + shift.y + 'px';
+      }
+
     }
+
+    function onPinUp(evtUp){
+      evtUp.preventDefault();
+      document.removeEventListener('mousemove', onPinMove);
+      document.removeEventListener('mouseup', onPinUp);
+    }
+
     document.addEventListener('mousemove', onPinMove);
     document.addEventListener('mouseup', onPinUp);
   });
